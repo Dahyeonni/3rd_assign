84e550fb6a54c7cdf36f9e9e400c9cb3a0ffb1c4
Dahyeonni <youlove7r@gmail.com>
2022-07-26 17:48:36 +0900

    싫어요

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from msilib.schema import Property
 
 # Create your models here.
 class Post(models.Model):
@@ -9,10 +10,23 @@ class Post(models.Model):
     pub_date = models.DateTimeField()
     body  = models.TextField()
     image= models.ImageField(upload_to="post/", blank=True, null=True)
+    like_user_set = models.ManyToManyField(User, blank=True, related_name='likes_user_set',through='Like')
+    dislike_user_set = models.ManyToManyField(User, blank=True, related_name="dislike_user_set", through="Dislike")
+
+    @property
+    def like_count(self):
+        return self.like_user_set.count()
+
+    @property
+    def dislike_count(self):
+        return self.dislike_user_set.count()
+
     def __str__(self):
-         return self.title
+        return self.title
+
     def summary(self):
-         return self.body[:20]
+        return self.body[:20]
+
 
 class Comment(models.Model):
     content = models.TextField()
@@ -21,4 +35,21 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
     id = models.AutoField(primary_key=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together =(('user', 'post'))
+
+class Dislike(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+          unique_together = (('user', 'post'))
 
